82fb6ac2bee6b60588bda5fb93c0b0e77943fe83
JingwenTian <mini.mosquitor@gmail.com>
2017-06-01 13:03:35 +0800

    v2.0 init

--- public/index.php
+++ public/index.php
@@ -1,80 +1,21 @@
 <?php
 
-/**
- * Define some constants
- */
+ini_set('date.timezone','Asia/Shanghai');
 define("DS", DIRECTORY_SEPARATOR);
 define("ROOT", realpath(dirname(__DIR__)) . DS);
-define("VENDORDIR", ROOT . "vendor" . DS);
-define("ROUTEDIR", ROOT . "routers" . DS);
-define("TEMPLATEDIR", ROOT . "templates" . DS);
 
-/**
- * Include autoload file
- */
-if (file_exists(VENDORDIR . "autoload.php")) {
-    require VENDORDIR . "autoload.php";
-} else {
-    die("<pre>Run 'php composer.phar install' in root dir</pre>");
-}
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+*/
 
-/**
- * Include bootstrap & config file
- */
-require ROOT . DS . 'bootstrap.php';
-require ROOT . 'config' . DS . 'config.php';
+$app = require __DIR__ . '/../src/bootstrap.php';
 
-/**
- * If user is not logged in, he's redirected to login page
- *
- * @param $app
- * @param $settings
- * @return callable
- */
-$authenticate = function($app) {
-    return function() use ($app) {
-        if (!isset($_SESSION['user'])) {
-            $app->flash('error', 'Login required');
-            $app->redirect('/admin/login');
-        }
-    };
-};
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+*/
 
-/**
- * If user is logged in, he are not able to visit register page, login page and will be
- * redirected to admin home
- *
- * @param $app
- * @param $settings
- * @return callable
- */
-$isLogged = function($app) {
-    return function() use ($app) {
-        if (isset($_SESSION['user'])) {
-            $app->redirect('/admin');
-        }
-    };
-};
-
-/**
- * Slim extension to implement fast JSON API's
- */
-function APIrequest(){
-    $app = \Slim\Slim::getInstance();
-    $app->view(new \JsonApiView());
-    $app->add(new \JsonApiMiddleware());
-}
-
-
-/**
- * Include all files located in routes directory
- */
-$routers = glob(ROUTEDIR . '*.router.php');
-foreach ($routers as $router) {
-    require $router;
-}
-
-/**
- * Run the application
- */
 $app->run();
--- routers/api.router.php
+++ routers/api.router.php
@@ -1,46 +1,7 @@
-<?php
+<?php 
 
-/**
- * slim-jsonAPI
- * [example Code]
- * $app->get('/', function() use ($app) {
- *       $app->render(200,array(
- *               'msg' => 'welcome to my API!',
- *           ));
- *   });
- */
-
-use lib\Common;
-use lib\Tools;
-
-//regular html response
-$app->get('/api', function () use ($app) {
-   
-    $app->log->info('this is api index router');   
-    $app->render('index.html', array('hello' => 'welcome to my API!'));
-    
-});
-
-//this request will have full json responses
-$app->get('/api/user/:id','APIrequest',function($id) use($app){
-
-    //your code here
-    
-    $app->render(200,array(
-        'msg' => 'user found',
-    ));
-
-    //Errors
-    // $app->render(404,array(
-        // 'error' => TRUE,
-        // 'msg'   => 'user not found',
-    // ));
-    
-    // if(...) {
-        // throw new Exception("Something wrong with your request!");
-    // }
-    
-    
-});
-
-
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+*/
