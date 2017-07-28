e603308983ae48dfb28139c03b5794d5f70925df
jingwentian <mini.mosquitor@gmail.com>
2017-07-28 16:36:04 +0800

    demo

--- app/controller/frontend/HomeController.php
+++ app/controller/frontend/HomeController.php
@@ -8,18 +8,20 @@ class HomeController extends BaseController
 
     public function home($request, $response, $args) 
     {
+        //echo $request->getAttribute('ip_address');
     	//$this->container->logger->info('hello world');
 
         // $attrs = $request->getAttributes();
         // echo $attrs['locale'];
         
-        // $this->container->cache->set("cache_key", "hello slim");
-        // echo $this->container->cache->get("cache_key");
+        // $this->container->cache->set("cache_key1", "hello slim");
+        // echo $this->container->cache->get("cache_key1");
 
     	//echo trans('home.hello', ['%name%' => 'Slim']);
 
         //return $this->container->view->render($response, 'frontend/home/index.phtml', ['translator' => $this->container->translator]);
-        return view($response, 'frontend/home/index.phtml', ['translator' => $this->container->translator]);
+        return view($response, 'frontend/home/index.phtml', ['translator' => $this->container->translator]);   
+
     }
 
 
